4245425540dce60f10cda43b3e08aa6b545501a3
Alexander Bayer <onebarloop@posteo.de>
2024-12-27 13:24:55 +0100

    Revert "add migrations"
    This reverts commit 754c6200d4c2d6eb31c3380494364a109b6405ed.

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,12 +1,4 @@
 import { drizzle } from 'drizzle-orm/vercel-postgres';
-import { migrate } from 'drizzle-orm/vercel-postgres/migrator';
-
-const db = drizzle();
-
-async function main() {
-  await migrate(db, { migrationsFolder: './drizzle' });
-}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-
-main();
+const db = drizzle();
