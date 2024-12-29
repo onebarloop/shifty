39b6f9b5b5cd4d1f1d9fc0b20e0da03930785880
Alexander Bayer <onebarloop@posteo.de>
2024-12-29 19:54:54 +0100

    env naming

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
-import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
-import { VercelPgDatabase } from 'drizzle-orm/vercel-postgres/driver';
+import type { VercelPgDatabase } from 'drizzle-orm/vercel-postgres/driver';
 
 export let db: NodePgDatabase<typeof schema> | VercelPgDatabase<typeof schema>;
 
-if (process.env.ENVIRONMENT === 'local') {
+if (process.env.DB_DRIVER === 'node_postgres') {
   const { drizzle } = await import('drizzle-orm/node-postgres');
   db = drizzle(process.env.POSTGRES_URL!, { schema });
 } else {
